98ea5e891a5e80f2f01e4d249ef3b7985da93f71
Alessandro Carotenuto <alex1499@gmail.com>
2026-03-02 01:07:08 +0100

    MLP structure begins

---
+++ mlp.py
@@ -0,0 +1,20 @@
+import torch
+import torch.nn as nn
+
+class Multi_Layer_Perceptron(nn.Module):
+
+    def __init__(
+            self,
+            input_dim : int,
+            output_dim : int,
+            hidden_dims : List[int],
+            activation_per_layer : str = 'relu'
+    ):
+        super.__init__()
+        pass
+
+    def forward():
+        pass
+
+
+
