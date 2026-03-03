567a650b5443dc70f57fb72a497ccea71d56b576
Alessandro Carotenuto <alex1499@gmail.com>
2026-03-03 23:36:33 +0100

    Added tests

--- model_tests.py
+++ model_tests.py
@@ -21,11 +21,31 @@ def MLP_class_test():
     print(" -> Model Created")
 
     batch_size = 5
-    dummy_input = torch.randn(batch_size,input_dim)
+    dummy_input = torch.randn(batch_size,input_dim)               #CREATE one-single-batch of inputs 
     output = model(dummy_input)
 
     assert output.shape == (batch_size, output_dim), f"Wrong output shape: {output.shape}"
     print(" -> Shape Test Passed")
+    softmax_sum=output.sum(dim=-1)
+
+    assert torch.allclose(softmax_sum, torch.ones(batch_size), atol=1e-6), f"Wrong output sum: {softmax_sum}, expected around one"
+    print(" -> Softmax Test Passed with 1e-6 tol")
+
+    assert torch.isfinite(output).all(), "Outpu t contains NaN or Inf"
+    print(" -> NaN/Inf Test Passed")
+
+    fakeloss=output.sum() #number connected to computational graph
+    fakeloss.backward()
+
+    for name, p in model.named_parameters():
+            assert p.grad is not None, f"No grad for {name}"
+            assert torch.isfinite(p.grad).all(), f"NaN/Inf grad in {name}"
+    print(" -> Gradient Test Passed")
+
+    assert model.total_params > 0, "No parameters"
+    assert model.trainable_params > 0, "No trainable parameters"
+    print(" -> Params Test Passed")
+    
 
 
 if __name__ == "__main__":
